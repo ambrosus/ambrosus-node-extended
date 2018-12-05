86d381ccecc5bd971994ffa5c639eb8ede88d06c
Conor Seabrook <conor.seabrook@gmail.com>
2018-12-05 17:49:01 -0500

    Amb price, bundle price, address balance

--- src/controller/metric.controller.ts
+++ src/controller/metric.controller.ts
@@ -1,19 +1,43 @@
+import axios from 'axios';
 import * as express from 'express';
 import { controller, httpGet, response } from 'inversify-express-utils';
 import * as Prometheus from 'prom-client';
+import { inject } from 'inversify';
+import { MIDDLEWARE, TYPE } from '../constant/types';
+import { authorize } from '../middleware/authorize.middleware';
+import { Web3Service } from '../service/web3.service';
 
-@controller('/metrics')
+@controller('/metrics', MIDDLEWARE.Context)
 export class MetricController {
-
-  constructor() {
+  constructor(@inject(TYPE.Web3Service) private web3Service: Web3Service) {
     Prometheus.collectDefaultMetrics({ timeout: 10000 });
   }
 
-  @httpGet(
-    '/'
-  )
+  @httpGet('/')
   public get(@response() res: express.Response) {
     res.set('Content-Type', Prometheus.register.contentType);
     res.end(Prometheus.register.metrics());
+  }
+
+  @httpGet('/amb', authorize('super_account'))
+  public async getAmb() {
+    const amb = await axios.get(
+      'https://api.coinmarketcap.com/v1/ticker/amber/'
+    );
+    return amb.data[0];
+  }
+
+  @httpGet('/bundle', authorize('super_account'))
+  public async getBundle() {
+    const bundleInfo = {
+      price: 12.0,
+    };
+    return bundleInfo;
+  }
+
+  @httpGet('/balance', authorize('super_account'))
+  public async getBalance() {
+    const balance = await this.web3Service.getBalance();
+    return { balance };
   }
 }
