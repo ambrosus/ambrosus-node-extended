883c8d45c4310cbf00b51692a6c80000ea7a6bf5
Lazar Eric <lazareric.com@gmail.com>
2018-12-07 19:12:10 +0200

    debug for parseAnalytics

--- src/controller/analytics.controller.ts
+++ src/controller/analytics.controller.ts
@@ -54,6 +54,10 @@ export class AnalyticsController extends BaseController {
   ): Promise<APIResponse> {
     const groupBy = timeSeriesGroupFromString(group);
 
+    console.log('****** DEBUG FOR getTimeRangeCountAggregate ******');
+    console.log(group);
+    console.log(typeof group);
+
     let count = await this.analyticsService.countForTimeRangeWithAggregate(
       collection,
       groupBy,
