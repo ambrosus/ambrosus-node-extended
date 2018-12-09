4e3d605ca420d3d9848da374879a99fd3d5f1c35
Conor Seabrook <conor.seabrook@gmail.com>
2018-12-09 10:14:37 -0500

    create asset view which contains organization ID.

--- src/database/repository/asset.repository.ts
+++ src/database/repository/asset.repository.ts
@@ -9,6 +9,49 @@ import { BaseRepository } from './base.repository';
 export class AssetRepository extends BaseRepository<Asset> {
   constructor(@inject(TYPE.DBClient) protected client: DBClient) {
     super(client, 'assets');
+
+    client.events.on('dbConnected', () => {
+      client.db.createCollection('assetsView', {
+        viewOn: 'assets',
+        pipeline: [
+          {
+            $project: {
+              _id: 0,
+              assets: '$$ROOT',
+            },
+          },
+          {
+            $lookup: {
+              localField: 'assets.content.idData.createdBy',
+              from: 'accounts',
+              foreignField: 'address',
+              as: 'accounts',
+            },
+          },
+          {
+            $unwind: {
+              path: '$accounts',
+              preserveNullAndEmptyArrays: true,
+            },
+          },
+          {
+            $project: {
+              _id: '$assets._id',
+              content: '$assets.content',
+              assetId: '$assets.assetId',
+              metadata: '$assets.metadata',
+              repository: '$assets.repository',
+              organization: '$accounts.organization',
+            },
+          },
+          {
+            $sort: {
+              'content.idData.timestamp': -1,
+            },
+          },
+        ],
+      });
+    });
   }
 
   get timestampField(): string {
