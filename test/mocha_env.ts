541a977eb80995bf0e56724a55c0a0b73fa59d8c
Conor Seabrook <conor.seabrook@gmail.com>
2018-12-20 16:09:08 -0500

    moved env vars to mocha_env.ts

---
+++ test/mocha_env.ts
@@ -0,0 +1,22 @@
+process.env.NODE_ENV = 'test';
+process.env.LOG_LEVEL = 'error';
+process.env.PAGINATION_MAX = '50';
+process.env.PAGINATION_DEFAULT = '10';
+process.env.WEB3_RPC = 'https://network.ambrosus.com';
+process.env.WEB3_NODEPRIVATEKEY =
+  '0xf05f6b45d9b615a21a4445a03d45b087608290cd436d6d836e2344dc7a715346';
+process.env.MONGO_HOSTS = 'localhost:27017';
+process.env.MONGO_DB_NAME = 'hermes';
+process.env.MONGO_REPLICA_SET = '';
+process.env.MONGO_USER = '';
+process.env.MONGO_PASSWORD = '';
+process.env.SENTRY_DSN = '';
+process.env.DASHBOARD_URL = 'https://dashboard.ambrosus.com';
+process.env.EMAIL_FROM = 'no-reply@ambrosus.com';
+process.env.EMAIL_ORGREQ_TO = 'conor.seabrook@ambrosus.com';
+process.env.EMAIL_API_KEY =
+  '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';
+process.env.EMAIL_TMPL_ID_INVITE = 'd-0f9b5646779e4e41a62e682f659c3350';
+process.env.EMAIL_TMPL_ID_ORGREQ = 'd-f3a2760da0cb479983bf1ac9d198457e';
+process.env.EMAIL_TMPL_ID_ORGREQ_APPROVE = 'd-7cdad568a2c1497ca8a632169b8b3b54';
+process.env.EMAIL_TMPL_ID_ORGREQ_REFUSE = 'd-67e083722c7d4c2291b82d038e8211f9';
