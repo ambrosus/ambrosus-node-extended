808dbf2ad3a70eee5d400d49e584116775b00902
seag <37109339+seagiv@users.noreply.github.com>
2020-07-23 03:07:30 -0400

    Asset fill organization (#131)
    * asset and event organizationId migration
    
    * linter fix

---
+++ src/migrations/20200723000001_fill_asset_organizationId.ts
@@ -0,0 +1,38 @@
+/*
+Copyright: Ambrosus Inc.
+Email: tech@ambrosus.com
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+
+// eslint-disable-next-line import/prefer-default-export
+const up = async (db, config, logger) => {
+  const assets = await db.collection('assets').find({organizationId: {$exists: false}});
+
+  let fillCount = 0;
+
+  const fillOrganizationId = async (asset) => {
+    console.log(`updatingAsset(organizationId): ${asset.assetId}`);
+
+    const account = await db.collection('accounts').findOne({ address: asset.content.idData.createdBy });
+
+    if (account !== null) {
+     await db.collection('assets').updateOne(
+      {assetId: asset.assetId},
+      {$set : {organizationId: account.organization}}
+     );
+
+     fillCount = fillCount + 1;
+    }
+  };
+
+  while (await assets.hasNext()) {
+    await fillOrganizationId(await assets.next());
+  }
+
+  logger.info(`Filled organizationIds for ${fillCount} assets`);
+};
+
+module.exports = { up };
---
+++ src/migrations/20200723000002_fill_event_organizationId.ts
@@ -0,0 +1,38 @@
+/*
+Copyright: Ambrosus Inc.
+Email: tech@ambrosus.com
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+
+// eslint-disable-next-line import/prefer-default-export
+const up = async (db, config, logger) => {
+  const events = await db.collection('events').find({organizationId: {$exists: false}});
+
+  let fillCount = 0;
+
+  const fillOrganizationId = async (event) => {
+    console.log(`updatingEvent(organizationId): ${event.eventId}`);
+
+    const account = await db.collection('accounts').findOne({ address: event.content.idData.createdBy });
+
+    if (account !== null) {
+     await db.collection('events').updateOne(
+      {eventId: event.eventId},
+      {$set : {organizationId: account.organization}}
+     );
+
+     fillCount = fillCount + 1;
+    }
+  };
+
+  while (await events.hasNext()) {
+    await fillOrganizationId(await events.next());
+  }
+
+  logger.info(`Filled organizationIds for ${fillCount} events`);
+};
+
+module.exports = { up };
