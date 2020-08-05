312f8f3c1fb600c4d3df3d56c355fdae3a39f654
seag <37109339+seagiv@users.noreply.github.com>
2020-08-05 02:51:18 -0400

    Event index migration (#137)
    * migration for events index
    
    * events index

---
+++ src/migrations/20200805000001_create_events_index.ts
@@ -0,0 +1,13 @@
+/*
+Copyright: Ambrosus Inc.
+Email: tech@ambrosus.com
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+
+// eslint-disable-next-line import/prefer-default-export
+export const up = async (db, config, logger) => {
+  await db.collection('events').createIndex({ 'content.idData.timestamp': -1, '_id': -1 });
+};
